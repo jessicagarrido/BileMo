3f0c098f4d8a6f81642ec42a32df58b11c9d531a
Jessica Garrido <jessica.garrido.di@gmail.com>
2025-02-01 20:06:03 +0100

    add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,66 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Clients;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Users;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $clients = [
+
+        ];
+
+        $client = new Clients();
+        $client->setUsername('Free')
+            ->setPassword($this->passwordHasher->hashPassword($client, 'bilemo2025'))
+            ->setRoles(['ROLE_ADMIN'])
+            ->setEmail('free@gmail.fr')
+            ->setDateCreate(new \DateTime());
+        $manager->persist($client);
+        $clients[] = $client;
+        $client = new Clients();
+        $client->setUsername('SFR')
+            ->setPassword($this->passwordHasher->hashPassword($client, 'bilemo2025'))
+            ->setRoles(['ROLE_ADMIN'])
+            ->setEmail('sfr@gmail.fr')
+            ->setDateCreate(new \DateTime());
+        $manager->persist($client);
+        $clients[] = $client;
+        $client = new Clients();
+        $client->setUsername('Orange')
+            ->setPassword($this->passwordHasher->hashPassword($client, 'bilemo2025'))
+            ->setRoles(['ROLE_ADMIN'])
+            ->setEmail('orange@gmail.fr')
+            ->setDateCreate(new \DateTime());
+        $manager->persist($client);
+        $clients[] = $client;
+
+        for ($i = 1; $i < 20; $i++) {
+            $user = new Users();
+            $user->setUsername('username_' . $i)
+                ->setPassword($this->passwordHasher->hashPassword($user, 'bilemo2025'))
+                ->setRoles(['ROLE_USER'])
+                ->setEmail('username_' . $i . '@gmail.fr')
+                ->setLastname('lastname_' . $i)
+                ->setFirstname('firstname_' . $i)
+                ->setDateCreate(new \DateTime())
+                ->setClient($clients[rand(0, count($clients) - 1)]);
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
